76fba0412fb4cb866bc00632ae81d214b72cd4b7
nicolas96 <nicolasflores96@gmail.com>
2022-04-27 19:34:25 -0300

    master

--- src/components/header/HeaderSocials.jsx
+++ src/components/header/HeaderSocials.jsx
@@ -8,7 +8,7 @@ import {FaGithub} from 'react-icons/fa'
 const HeaderSocials = () => {
     return (
         <div className='header__socials'>
-            <a href="https://www.instagram.com/kevflores96/" target="_blank"><GrInstagram size="1.6rem" /></a>
+            <a href="https://www.instagram.com/kevflores._/" target="_blank"><GrInstagram size="1.6rem" /></a>
             <a href="https://twitter.com/KevFlores96" target="_blank"><FaTwitter size="1.6rem"/></a>
             <a href="https://linkedin.com/in/kevin-flores96" target="_blank"><BsLinkedin size="1.3em"/></a>
             <a href="https://github.com/KevNic96" target="_blank"><FaGithub size="1.3em"/></a>
